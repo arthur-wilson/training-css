9dd9602c2eb1b3b4cd4b36797c369e74d408fa1b
Arthur Wilson <contact.arthurwilson@gmail.com>
2022-07-17 21:32:26 -0300

    added funcionality of clearing input values

--- form-03/code/preventSubmit.js
+++ form-03/code/preventSubmit.js
@@ -1,5 +1,7 @@
 const formSubmit = document.getElementById("formSubmit");
 
 formSubmit.addEventListener('click', function(event) {
+    document.getElementById("userName").value = "";     // Clening input value
+    document.getElementById("userPassword").value = ""; // Cleaning input value
     event.preventDefault();
 });
